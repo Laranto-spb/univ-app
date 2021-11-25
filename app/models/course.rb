5906fcb07b1d3a9e82682cb9e07fc00e53e60737
Larisa Antokhina <laranto1985@gmail.com>
2021-11-25 15:55:23 +0300

    Course model: listing page from db, validation,  styling: boostrap icons, listing view

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,7 @@
 class Course < ApplicationRecord
+
+  validates :short_name, presence: true, length: {minimum: 4, maximum: 6}
+  validates :name, presence: true, length: {minimum: 6, maximum: 40}
+  validates :description, presence: true, length: {minimum: 10, maximum: 100}
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   
   root 'courses#index'
 
+  resources :courses
+
 end
